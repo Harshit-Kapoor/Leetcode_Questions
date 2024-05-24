0480145eaccddd1a1b55d2da9997b0178cfa9300
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2024-05-24 21:42:47 +0530

    Time: 1 ms (99.81%), Space: 43.1 MB (72.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,37 +1,57 @@
 class Solution {
-        public static boolean isAnagram(String s, String t) {
+//         public static boolean isAnagram(String s, String t) {
 
-        if (s.length() != t.length()) {
-            return false;
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> fhm = new HashMap<Character, Integer>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             fhm.put(ch, fhm.getOrDefault(ch, 0) + 1);
+//         }
+
+//         for (int i = 0; i < t.length(); i++) {
+//             char ch = t.charAt(i);
+
+//             if(fhm.containsKey(ch)) {
+
+//                 if(fhm.get(ch) == 1) {
+//                     fhm.remove(ch);
+//                 } else {
+//                     fhm.put(ch, fhm.get(ch) - 1);
+//                 }
+
+//             }
+
+//         }
+
+//         if (fhm.size() == 0) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+
+//     }
+    
+        public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
         }
-
-        HashMap<Character, Integer> fhm = new HashMap<Character, Integer>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            fhm.put(ch, fhm.getOrDefault(ch, 0) + 1);
+        
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-
-            if(fhm.containsKey(ch)) {
-
-                if(fhm.get(ch) == 1) {
-                    fhm.remove(ch);
-                } else {
-                    fhm.put(ch, fhm.get(ch) - 1);
-                }
-
+        
+        for (int n : freq) {
+            if (n != 0) {
+                return false;
             }
-
         }
-
-        if (fhm.size() == 0) {
-            return true;
-        } else {
-            return false;
-        }
-
+        
+        return true;
     }
 }
