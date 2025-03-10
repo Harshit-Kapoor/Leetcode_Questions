4b1305ea3d75ffb96e6dd5cc6e8e76d7fa3c268a
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-03-11 00:01:31 +0530

    Time: 27 ms (85.17%), Space: 52.9 MB (14.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,46 +1,57 @@
 class Solution {
-        public static List<List<Integer>> threeSum(int[] nums) {
-        
+    public static List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        int initialPointer = 0;
-        List<List<Integer>> mainList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
 
-        while (initialPointer < nums.length - 1) {
+            int n1 = nums[i];
+            int target = -n1;
 
-            int i = initialPointer + 1;
-            int j = nums.length - 1;
-            int n1 = -nums[initialPointer];
-
-            if (initialPointer > 0 && nums[initialPointer] == nums[initialPointer - 1]) {
-                initialPointer++;
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            while (i < j) {
+            twoSum(nums, target, i + 1, nums.length - 1, result);
 
-                if (nums[i] + nums[j] < n1) {
-                    i++;
-                } else if (nums[i] + nums[j] > n1) {
-                    j--;
-                } else { 
-                    List<Integer> tempList = new ArrayList<>();
-                    tempList.add(nums[initialPointer]);
-                    tempList.add(nums[i]);
-                    tempList.add(nums[j]);
-                    mainList.add(tempList);
-                    while (i < j && nums[i] == nums[i + 1]) i++;
-                    while (i < j && nums[j] == nums[j - 1]) j--;
-
-                    i++;
-                    j--;
-                }
-            }
-
-            initialPointer++;
         }
 
-        return mainList;
+        return result;
+
+    }
+
+    private static void twoSum(int[] nums, int target, int start, int end, List<List<Integer>> result) {
+
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+
+            if (nums[i] + nums[j] < target) {
+                i++;
+            } else if (nums[i] + nums[j] > target) {
+                j--;
+            } else if (nums[i] + nums[j] == target) {
+
+                // first we will remove duplicates
+
+                while (i < j && nums[i] == nums[i+1]) i++;
+                while (i < j && nums[j] == nums[j-1]) j--;
+
+                List<Integer> temp = new ArrayList<>();
+                temp.add(-target);
+                temp.add(nums[i]);
+                temp.add(nums[j]);
+                result.add(temp);
+                i++;
+                j--;
+            }
+
+        }
+
+
     }
 
 }
