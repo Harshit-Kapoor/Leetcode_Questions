0170124fe27b83f4ec7bdc111d5aa8809573ec3e
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-03-31 23:47:41 +0530

    Time: 33 ms (52.94%), Space: 62.8 MB (55.29%) - LeetHub

--- 3212-count-the-number-of-good-partitions/3212-count-the-number-of-good-partitions.java
+++ 3212-count-the-number-of-good-partitions/3212-count-the-number-of-good-partitions.java
@@ -1,14 +1,18 @@
 class Solution {
      public int numberOfGoodPartitions(int[] A) {
+        
         int res = 1, n = A.length, mod = 1000000007;
         Map<Integer, Integer> last = new HashMap<>();
+
         for (int i = 0; i < n; ++i) {
             last.put(A[i], i);
         }
+         
         for (int i = 0, j = 0; i < n; ++i) {
             res = i > j ? (res * 2) % mod : res;
-            j = Math.max(j, last.get(A[i]));
+            j = Math.max(j, last.get(A[i]));   // jha tak j jaa skta hai what tak lee jana hai get.A[i] se j could be reduced
         }
+
         return res;
     }
 }
