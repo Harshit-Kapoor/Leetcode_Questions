dbd0ca7fcb1d1b513edeb82544bd17c924c3f442
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-01 23:14:35 +0530

    Time: 19 ms (20.54%), Space: 48.9 MB (14.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,40 @@
 class Solution {
+
+    public String giveMeNewWord(String s) {
+
+        String newWord = "";
+
+        int[] arr = new int[26];
+
+        for (int i=0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (arr[i] > 0) {
+                newWord += (char) (i + 'a'); // Fix: Using correct index to create sorted word
+                arr[i]--;
+            }
+        }
+
+        return newWord;
+
+    }
+
+
     public List<List<String>> groupAnagrams(String[] strs) {
 
         HashMap<String, List<String>> map = new HashMap<>();
 
         for (String word : strs) {
 
-            char[] ch = word.toCharArray();
-            Arrays.sort(ch);
-            String sortedVal = String.valueOf(ch);
+            String newWord = giveMeNewWord(word);
 
-            if (!map.containsKey(sortedVal)) {
-                map.put(sortedVal, new ArrayList<>());
+            if (!map.containsKey(newWord)) {
+                map.put(newWord, new ArrayList<>());
             }
 
-            map.get(sortedVal).add(word);
+            map.get(newWord).add(word);
 
         }
 
