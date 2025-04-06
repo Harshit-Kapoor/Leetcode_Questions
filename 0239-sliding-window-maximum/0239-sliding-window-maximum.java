b18992c9aecc59678648d0a0310b3d7bd3c777e0
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-06 22:35:12 +0530

    Time: 31 ms (66.92%), Space: 58.4 MB (77.16%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,32 +1,45 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        int j=0,i=0,maxsum=Integer.MIN_VALUE,arrayIndex=0;
-        int[] x=new int[nums.length-k+1];
-        List<Integer> f=new ArrayList<>();
-        
-        
-        while(j<nums.length){
-            
-            while(f.size()!=0 && f.get(f.size()-1)<nums[j]){
-                f.remove(f.size()-1);
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+        // We will store index in deque not values
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        int n = nums.length;
+
+        // result size will be n - k + 1 since only n - k + 1 window can be created
+        int[] result = new int[n - k + 1];
+        int resultPointer = 0;
+
+        // we are create monotonic deque which will store indexes for values in decreasing order
+
+        // 4 steps honge mostly 
+
+        // step 1 = remove from dequeFront if element is out of window
+        // step 2 = remove from dequeBack if incoming element is greater then last of deque 
+        // step 3 = add current Element to back of back
+        // step 4 = store in result array only if i >= k - 1 ie when i becomes 2 for k = 3 from then we start storing 
+
+        for (int i=0; i<n; i++) {
+
+            // step 1 = remove from dequeFront if element is out of window
+            while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+                dq.pollFirst();
             }
-            f.add(nums[j]);
-            
-            
-            if(j-i+1<k){
-                j++;
+
+            // step 2 = remove from dequeBack if incoming element is greater then last of deque 
+            while (!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
+                dq.pollLast();
             }
-            else if(j-i+1==k){
-                x[arrayIndex]=f.get(0);
-                arrayIndex++;
-                if(nums[i]==f.get(0))f.remove(0);
-                i++;
-                j++;
-                
+
+            dq.addLast(i);
+
+            if (i >= k - 1) {
+                result[resultPointer++] = nums[dq.peekFirst()];
             }
+
         }
-        return x;
-        
+
+         return result;
+
     }
 }
