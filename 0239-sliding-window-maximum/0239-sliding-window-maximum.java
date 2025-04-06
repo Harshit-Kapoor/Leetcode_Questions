b08fa1dfde9008e6b828f771f53dde893b088930
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-06 23:02:12 +0530

    Time: 32 ms (53.31%), Space: 59.1 MB (53.02%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -15,18 +15,22 @@ class Solution {
         // 4 steps honge mostly 
 
         // step 1 = remove from dequeFront if element is out of window
-        // step 2 = remove from dequeBack if incoming element is greater then last of deque 
+        // step 2 = remove from dequeBack if incoming element is greater then element fetched from last of deque 
         // step 3 = add current Element to back of back
         // step 4 = store in result array only if i >= k - 1 ie when i becomes 2 for k = 3 from then we start storing 
 
         for (int i=0; i<n; i++) {
 
             // step 1 = remove from dequeFront if element is out of window
+
+            // hum check krenge if dq ke first element ka index has gone out of window if its < than i - k +1
+
+            // i-k+1 is my valid window starting point, usse kam ke koi bhi index nikal do
             while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
                 dq.pollFirst();
             }
 
-            // step 2 = remove from dequeBack if incoming element is greater then last of deque 
+            // step 2 = remove from dequeBack if incoming element is greater then element fetched from index in last of deque 
             while (!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
                 dq.pollLast();
             }
