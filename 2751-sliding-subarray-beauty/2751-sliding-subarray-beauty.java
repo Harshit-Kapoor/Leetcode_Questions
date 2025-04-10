b9b31dbe7338b8fe5725e8dc56bd7f78f11b0252
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-04-10 20:13:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.java
@@ -0,0 +1,47 @@
+class Solution {
+        public static int[] getSubarrayBeauty(int[] arr, int k, int x) {
+
+        int n = arr.length;
+        int[] ans = new int[n - k + 1];
+        int ansIndex = 0;
+        int i = 0;
+        int j = 0;
+        List<Integer> lst = new ArrayList<Integer>();
+
+        while (j < n) {
+
+            if (arr[j] < 0) {
+                lst.add(arr[j]);
+            }
+
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+
+                List<Integer> copy = new ArrayList<Integer>();
+
+                if (!lst.isEmpty()) {
+                    copy.addAll(lst);
+                    Collections.sort(copy);
+                    ans[ansIndex++] = copy.get(x - 1);
+
+                    if (arr[i] == lst.get(0)) {
+                        lst.remove(0);
+                    }
+                    copy.clear();
+                } else {
+                    ans[ansIndex++] = 0;
+                }
+
+                i++;
+                j++;
+
+
+            }
+        }
+
+        return ans;
+
+    }
+
+}
