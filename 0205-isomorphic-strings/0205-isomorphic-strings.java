6f6579f8aca6914b8b4ea212452503127e0de642
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2025-05-05 16:16:59 +0530

    Time: 11 ms (70.17%), Space: 43.1 MB (9.4%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,27 +3,31 @@ class Solution {
         
         HashMap<Character, Character> map = new HashMap<>();
 
-        for (int i=0; i<s.length(); i++) {
+        for (int i=0; i < s.length(); i++) {
 
             char originalChar = s.charAt(i);
             char replacedChar = t.charAt(i);
 
-            if (map.containsKey(originalChar)) {
-                if (map.get(originalChar) != replacedChar) {
-                    return false;
-                }
-            } else {
-                if (map.containsValue(originalChar)) {
-                    return false;
-                } else {
+            if (!map.containsKey(originalChar)) {
+
+                if (!map.containsValue(replacedChar)) {
                     map.put(originalChar, replacedChar);
+                } else {
+                    return false;
                 }
 
+            } else {
+
+                char temp = map.get(originalChar);
+                if (temp != replacedChar) {
+                    return false;
+                }
             }
 
         }
 
         return true;
+
         
     }
 }
